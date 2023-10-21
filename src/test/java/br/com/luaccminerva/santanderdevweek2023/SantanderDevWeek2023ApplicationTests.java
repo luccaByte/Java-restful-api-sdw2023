773957128e5d708c03d3c059457791d9ea28cebe
lucas <lucaston2323@gmail.com>
2023-10-20 21:19:49 -0300

    first commit

---
+++ src/test/java/br/com/luaccminerva/santanderdevweek2023/SantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luaccminerva.santanderdevweek2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
