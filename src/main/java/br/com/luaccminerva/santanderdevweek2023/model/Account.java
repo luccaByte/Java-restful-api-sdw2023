bf6e133e3f2bde11d8a834ec07ea766d54dcb6b6
lucas <lucaston2323@gmail.com>
2023-10-21 20:45:20 -0300

    add getters e setters nas classes

--- src/main/java/br/com/luaccminerva/santanderdevweek2023/model/Account.java
+++ src/main/java/br/com/luaccminerva/santanderdevweek2023/model/Account.java
@@ -18,12 +18,56 @@ public class Account {
     @Column(unique = true)
     private String number;
 
-
     private String agency;
 
     @Column( scale = 13, precision = 2)
     private BigDecimal balance;
 
-    @Column( scale = 13, precision = 2)
+
+    @Column(name = "additional_limit", scale = 13, precision = 2)
     private BigDecimal limit;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+    
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+    
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+
 }
--- src/main/java/br/com/luaccminerva/santanderdevweek2023/model/Card.java
+++ src/main/java/br/com/luaccminerva/santanderdevweek2023/model/Card.java
@@ -23,6 +23,47 @@ public class Card {
     @Column( scale = 13, precision = 2)
     private BigDecimal balance;
 
-    @Column( scale = 13, precision = 2)
+    @Column(name = "avaliable_limit", scale = 13, precision = 2)
     private BigDecimal limit;
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+    
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+    
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
 }
