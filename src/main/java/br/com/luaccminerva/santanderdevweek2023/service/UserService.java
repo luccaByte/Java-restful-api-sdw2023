1e03d8a5748453b31bfbd574c00481b9db8cb318
lucas <lucaston2323@gmail.com>
2023-10-22 17:49:20 -0300

    criando pkg service e implementando tratamento de erro

---
+++ src/main/java/br/com/luaccminerva/santanderdevweek2023/service/UserService.java
@@ -0,0 +1,10 @@
+package br.com.luaccminerva.santanderdevweek2023.service;
+
+import br.com.luaccminerva.santanderdevweek2023.model.User;
+
+public interface UserService {
+
+    User findById(Long id);
+
+    User create (User userToCreate);
+}
---
+++ src/main/java/br/com/luaccminerva/santanderdevweek2023/service/impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package br.com.luaccminerva.santanderdevweek2023.service.impl;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+
+import br.com.luaccminerva.santanderdevweek2023.model.User;
+import br.com.luaccminerva.santanderdevweek2023.repository.UserRepository;
+import br.com.luaccminerva.santanderdevweek2023.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl (UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("O número da Account já existe.");
+        }
+
+        return userRepository.save(userToCreate);
+    }
+    
+}
